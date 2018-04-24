70b175c8fc69ccf1e0a6849426addd5597191829
h01m3s <lin426797@gmail.com>
2018-04-24 00:06:35 -0400

    Serve HTML Files and POST Requests

--- nodejs_restapi/app.js
+++ nodejs_restapi/app.js
@@ -4,18 +4,45 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('./public'))
 
 app.use(morgan('short'))
 
-app.get('/user/:id', (req, res) => {
-    console.log("Fetching user with id: " + req.params.id)
+app.post('/user_create', (req, res) => {
+    console.log("Trying to create a new user...")
+    console.log("First name: " + req.body.create_first_name)
+    const firstName = req.body.create_first_name
+    const lastName = req.body.create_last_name
 
-    const connection = mysql.createConnection({
+    const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
+    getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
+        if (err) {
+            console.log("Failed to insert new user: "+ err)
+            res.sendStatus(500)
+            return
+        }
+        console.log("Inserted a new user with id: ", results.insertId)
+        res.end()
+    })
+})
+
+function getConnection() {
+    return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         // password: ''
         database: 'test_mysql'
     })
+}
+
+app.get('/user/:id', (req, res) => {
+    console.log("Fetching user with id: " + req.params.id)
+
+    const connection = getConnection()
 
     const userId = req.params.id
     const queryString = "SELECT * FROM users WHERE id = ?"
