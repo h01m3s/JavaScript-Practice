f47bdc1f6282cfb2014da66225da3081ffdee334
h01m3s <lin426797@gmail.com>
2019-05-02 20:43:23 -0400

    Promise added.

---
+++ Promise/script.js
@@ -0,0 +1,16 @@
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1;
+  if (a == 2) {
+    resolve("Success");
+  } else {
+    reject("Failed");
+  }
+});
+
+p.then(message => {
+  // when resolve
+  console.log("This is in the then " + message);
+}).catch(message => {
+  // when reject
+  console.log("This is in the catch " + message);
+});
