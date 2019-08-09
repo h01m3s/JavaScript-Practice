8398403103302edaced3f077e691fea499650138
h01m3s <lin426797@gmail.com>
2019-08-08 23:26:53 -0400

    Const_let_1.js update

--- ES6/const_let_1.js
+++ ES6/const_let_1.js
@@ -4,3 +4,15 @@ const foo;
 // Error: Syntax Error, variable cannot be re-assigned.
 const foo = 'hello';
 foo = 'world';
+
+// Block scope
+function fn() {
+
+  const foo = 'hello';
+  if (true) {
+    const foo = 'world';
+    console.log(foo);     // print 'world
+  }
+  console.log(foo);       // print 'hello'
+
+}
